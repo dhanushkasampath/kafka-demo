f806d17376860b152026213c5141ed91449a2e84
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-10 14:20:37 +0530

    initial setup

---
+++ src/test/java/com/learn/kafka_demo/KafkaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.kafka_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
